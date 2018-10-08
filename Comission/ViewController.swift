48575522fe4757b9732c3c436d9b8cd84e7630de
Samuel Schuffler <sschuffler1332@jh-mba158-22.local>
2018-10-08 14:32:07 -0500

    Initial Commit

---
+++ Comission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comission
+//
+//  Created by Samuel Schuffler on 10/8/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
